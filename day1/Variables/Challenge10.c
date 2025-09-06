e7d564b980416b9b709d39e93e344684d927a6af
User <user@example.com>
2025-09-06 19:33:40 +0100

    Ajout du programme pour calculer le volume d'une sphere

---
+++ day1/Variables/Challenge10.c
@@ -0,0 +1,11 @@
+	#include <stdio.h>
+#include <math.h>
+#define pi 3.14
+	int main (){
+	float r ,v;   
+        printf("ecrire la r  :");
+	    scanf("%f",&r);
+            v=(4.00/3.00) * pi * pow(r,3);
+            printf("la volume est :%f",v);
+            return 0;
+}
