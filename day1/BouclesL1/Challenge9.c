868ad145e4b0ef60f1072b853ba077be0336a22d
User <user@example.com>
2025-09-07 23:22:57 +0100

    Ajout du programme compteur de chiffres

---
+++ day1/BouclesL1/Challenge9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, compteur = 0;
+
+    printf("Entrez un entier positif : ");
+    scanf("%d", &n);
+
+    if (n == 0) {
+        compteur = 1;
+    } else {
+        while (n != 0) {
+            n = n / 10;       
+            compteur++;    
+        }
+    }
+
+    printf("Nombre de chiffres = %d\n", compteur);
+
+    return 0;
+}
