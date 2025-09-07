0e0cca94ff1f66976eb7fb91ebe0759882e0cc10
User <user@example.com>
2025-09-07 12:13:05 +0100

    Ajout du programme Mention Obtenue avec vérification des notes

--- day1/ConditionsL1/Challenge8.c
+++ day1/ConditionsL1/Challenge8.c
@@ -13,7 +13,7 @@ int main() {
         printf("Mention : Passable\n");
     } 
     else if (moyenne >= 12 && moyenne < 14) {
-        printf("Mention : Assez Bien\n");
+        printf("Mention : Assez Bien \n");
     }
      else if (moyenne >= 14 && moyenne < 16) {
         printf("Mention : Bien\n");
