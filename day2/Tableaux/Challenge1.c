1d41424c81a1c7fd7ee848a99127a8a5a03ae284
User <user@example.com>
2025-09-07 23:38:25 +0100

    Ajout du programme affichage d'un tableau en C

---
+++ day2/Tableaux/Challenge1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int tableau[5] = {1, 2, 3,4,5};
+
+    // Affichage 
+    printf("%d\n", tableau[0]);
+    printf("%d\n", tableau[1]);
+    printf("%d\n", tableau[2]);
+    printf("%d\n", tableau[3]);
+    printf("%d\n", tableau[4]);
+
+    // Affichage  boucle
+    printf("Contenu du tableau :\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", tableau[i]);
+    }
+
+    return 0;
+}
+
+
+
+    
+
+ 
+
