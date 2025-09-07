9533ff4b67ea2e5e6996debfc022ffef57e5d249
User <user@example.com>
2025-09-07 10:38:13 +0100

    Ajout du programme somme ou triple selon égalité

---
+++ day1/ConditionsL1/Challenge3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    int a,b;
+    printf("ecrire la nomber a :");
+    scanf("%i",&a);
+    printf("ecrire la nomber b :");
+    scanf("%i",&b);
+    if (a != b)
+    {
+        printf("la somme de nomber a et b : %i",a+b);
+    }else{
+            printf("la troie de nomber a et b : %i",a*b*a);
+
+    }
+    
+    
+}
